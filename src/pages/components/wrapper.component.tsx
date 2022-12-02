4dbdb6ce3dafa37edefd0fde3cac955764109900
Rafael <rafael.estevamdesign@gmail.com>
2022-12-01 22:09:45 -0300

    Site

--- src/pages/components/wrapper.component.tsx
+++ src/pages/components/wrapper.component.tsx
@@ -1,4 +1,8 @@
-const Wrapper = ({children}) => {
+interface WrapperChildren {
+    children: React.ReactNode
+}
+
+const Wrapper = ({children}:WrapperChildren) => {
     return (
         <div style={{width: '100%'}}>{children}</div>
     )
