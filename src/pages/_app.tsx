8842b41349d831aa9b63b6d8e5a8dd0bd2f38c41
Rafael Oliveira <rafael.oliveira@printi.com.br>
2023-11-23 16:02:02 -0300

    OLD GTM

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,18 @@
-import "../styles/globals.css";
-import { ThemeProvider } from "styled-components";
-import type { AppProps } from "next/app";
-import GlobalStyle from "../styles/global";
-import { SnackbarProvider } from "notistack";
-import Script from "next/script";
+import '../styles/globals.css'
+import { ThemeProvider } from 'styled-components'
+import type { AppProps } from 'next/app'
+import GlobalStyle from '../styles/global'
+import { SnackbarProvider } from 'notistack'
 
-import theme from "../styles/theme";
+import theme from '../styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-YZMSNSM2C6"
-      />
-      <SnackbarProvider maxSnack={3}>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </SnackbarProvider>
-    </>
-  );
+    <SnackbarProvider maxSnack={3}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SnackbarProvider>
+  )
 }
