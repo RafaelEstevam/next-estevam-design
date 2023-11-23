16f377f6ff1953c89ec1bf6e4838ae6308731247
Rafael Oliveira <rafael.oliveira@printi.com.br>
2023-11-23 16:00:02 -0300

    SCRIPT GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,25 @@
-import '../styles/globals.css'
-import { ThemeProvider } from 'styled-components'
-import type { AppProps } from 'next/app'
-import GlobalStyle from '../styles/global'
-import { SnackbarProvider } from 'notistack'
+import "../styles/globals.css";
+import { ThemeProvider } from "styled-components";
+import type { AppProps } from "next/app";
+import GlobalStyle from "../styles/global";
+import { SnackbarProvider } from "notistack";
+import Script from "next/script";
 
-import theme from '../styles/theme'
+import theme from "../styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SnackbarProvider maxSnack={3}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </SnackbarProvider>
-  )
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-YZMSNSM2C6"
+      />
+      <SnackbarProvider maxSnack={3}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </SnackbarProvider>
+    </>
+  );
 }
