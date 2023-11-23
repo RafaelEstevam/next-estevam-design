1bb9934ae558582ca48ba06e636ae30473a076a2
Rafael Oliveira <rafael.oliveira@printi.com.br>
2023-11-23 14:59:42 -0300

    FIX GTM

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,24 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <SnackbarProvider maxSnack={3}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <head>
-
-        </head>
-        <script id="google-tag-manager">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-52BFJFHD');
-          `}
-        </script>
         <Component {...pageProps} />
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-52BFJFHD" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-          }}
-        />
       </ThemeProvider>
     </SnackbarProvider>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
 import { GetStaticProps } from "next";
-import { createContext } from 'react';
+import { createContext, useEffect } from 'react';
 import { GetApi } from '../services/api';
 import LandingPageTemplate, { ContentProps } from './templates/landingPage.template';
 import { queryGetPage } from '../queries/getPage';
+import {datalayerInitializer} from './utils/datalayerInitializer';
 
 
 export const LanguageContext = createContext(null);
 
 export default function Home(props: ContentProps) {
   const { menus, contents, slides, graduations, experiences, technologies, networks, badgets, photos, words, cvs } = props;
+
+  useEffect(() => {
+    datalayerInitializer('GTM-52BFJFHD');
+  }, []);
 
   return (
     <LandingPageTemplate {...{ menus, contents, slides, graduations, experiences, technologies, networks, badgets, photos, words, lang:"en", cvs }} />
